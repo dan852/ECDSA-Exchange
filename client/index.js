0e70ee3ed3554365ff5b22615bffc5913fd3a142
dan852 <daniel.knapp@icloud.com>
2022-01-19 16:23:07 +0800

    initial commit

--- client/index.js
+++ client/index.js
@@ -1,5 +1,9 @@
 import "./index.scss";
 
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const SHA256 = require('crypto-js/sha256');
+
 const server = "http://localhost:3042";
 
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
@@ -19,16 +23,34 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const pk = document.getElementById("private-key").value;
 
-  const body = JSON.stringify({
-    sender, amount, recipient
-  });
+  const transaction = {
+    sender: sender, 
+    amount: amount, 
+    recipient: recipient
+  }
+
+  const key = ec.keyFromPrivate(pk);
+  const msgHash = SHA256(transaction);
+  const signature = key.sign(msgHash.toString());
+  
+  const body = JSON.stringify({ signature: {
+    r: signature.r.toString(16),
+    s: signature.s.toString(16)
+  }, transaction: transaction });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
   fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
+    if (!response.ok) {
+      return response.text().then(text => {throw new Error(text)})
+    }
     return response.json();
   }).then(({ balance }) => {
     document.getElementById("balance").innerHTML = balance;
+  }).catch(function(error) {
+    console.log(error);
+    alert(error);
   });
 });
--- server/index.js
+++ server/index.js
@@ -2,17 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
-
+const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
-}
+const balances = { };
+const accounts = { };
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
@@ -21,12 +20,49 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const {signature, transaction} = req.body;
+  const {sender, recipient, amount} = transaction;
+
+  const key = ec.keyFromPublic({x: accounts[sender].publicX, y: accounts[sender].publicY }, 'hex');
+  const msgHash = SHA256(transaction).toString();
+
+  if(balances[sender] < amount) {
+    res.status(403).send('Insufficient balance');
+    return;
+  }
+
+  if(key.verify(msgHash, signature)) {
+    balances[sender] -= amount;
+    balances[recipient] = (balances[recipient] || 0) + +amount;
+    res.send({ balance: balances[sender] });
+  } else {
+    res.status(401).send('Not Authenticated, check your PK!');
+  }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  console.log(`\nAvailable Accounts\n==================`);
+  for(let i = 0; i < 10; i++) {
+    let key = ec.genKeyPair();
+    let account = key.getPublic().encode('hex');
+    account = `0x${account.slice(-40)}`;
+
+    balances[account] = Math.floor(Math.random() * 10);
+    accounts[account] = {
+      privateKey: key.getPrivate().toString(16),
+      publicX: key.getPublic().x.toString(16),
+      publicY: key.getPublic().y.toString(16),
+    }
+
+    console.log(`(${ i })`, account, `(${ balances[account] } SHOT)`);
+  }
+
+  console.log(`\nPrivate Keys\n============`);
+  let i = 0;
+  for(const [key, account] of Object.entries(accounts)) {
+    console.log(`(${i}) ${ account.privateKey }`);
+    delete account.privateKey;
+    i++;
+  }
 });
